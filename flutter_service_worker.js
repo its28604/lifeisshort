f8c19ebd4d7993ff50e88f8800208f1577924431
黃煒傑 <wei-chieh.huang@s-MacBook-Air.local>
2023-12-16 11:18:01 +0800

    Fix the dialog not functional issue

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "88df11e6ef32ac7a98c97b26d6af3e06",
-"index.html": "fa28e578b20e8a50f1db5c2b8bfe5444",
-"/": "fa28e578b20e8a50f1db5c2b8bfe5444",
-"main.dart.js": "3d5464d1125fc5c2a5f6e237b0aa9d62",
+"index.html": "49ad434df0792d7722732bae33d48c27",
+"/": "49ad434df0792d7722732bae33d48c27",
+"main.dart.js": "05fc6ddc07420c80313c3a27227b3965",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
